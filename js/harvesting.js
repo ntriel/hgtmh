f301f73df579f62d8d796b5e9464a33dfcf27dfd
Jonah White <Jonah-White@hotmail.com>
2024-07-15 23:19:26 -0500

    V11 Update
    Bugged. You can only click one button not both. If we use Hook.on it works every time but .Once only works once no matter the button. However with Hook.On it takes up a bunch of processing power

--- js/harvesting.js
+++ js/harvesting.js
@@ -1,9 +1,6 @@
 
-Hooks.once("init", async function () {
-    school = game.dnd5e.config.spellSchools
-    school["bio"] = "Biomancy"
-
-    game.dnd5e.config.spellSchools = school
+Hooks.on("init", async function () {
+    
 
 });
 
@@ -48,141 +45,41 @@ let HARVEST_MESSAGE = `
 `;
 
 let d20 = "icons/svg/d20-grey.svg";
-let d20Hover = 'icons/svg/d20-highlight.svg';
+let d20Hover = 'icons/svg/d20-black.svg';
 
-Hooks.on('renderChatMessage', (chatItem, html) => {
-    // console.log(chatItem.data.content);
-    //console.log("html",html);
-    var parser = new DOMParser();
-    var htmlDoc = parser.parseFromString(chatItem.data.content, 'text/html');
-    let theElement = htmlDoc.evaluate(`//div[@data-hgtmh="true"]`, htmlDoc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-    if (theElement.singleNodeValue != null) {
-        //console.log("theElement.singleNodeValue", theElement.singleNodeValue);
-        //console.log("theElement.singleNodeValue.outerHTML", theElement.singleNodeValue.outerHTML);
-        let content = theElement.singleNodeValue.outerHTML.replaceAll(`data-hgtmh="true"`, `data-hgtmh="false"`);
-        let id1 = Math.floor(Math.random() * 100000000);
-        let id2 = Math.floor(Math.random() * 100000000);
-        let id3 = Math.floor(Math.random() * 100000000);
 
-        let creatureType = theElement.singleNodeValue.attributes["data-creatureType"].value;
-        let creatureName = theElement.singleNodeValue.attributes["data-creatureName"].value;
-		
-		
-		
-		let skillReq = getSkill(creatureType);
-        content = HARVEST_MESSAGE.replaceAll("{id1}", id1).replaceAll("{id2}", id2).replaceAll("{id3}", id3).replaceAll("{d20}", d20).replaceAll("{creatureType}", creatureType).replaceAll("{creatureName}", creatureName).replaceAll("{skillReq}",skillReq);
-		
-		
-        if (game.user.isGM) {
-            chatItem.update({
-                content: content
-            })
-        }
 
-    }
-
-    html.find(".hgtmh-roll-button").click(async e => {
-        //console.log(e);
-        if (e.currentTarget.attributes["data-rolled"].value == 'false') {
-			
-            let actor;
-            if (game.user.role == 4) {
-                actor = canvas.tokens.controlled[0].actor;
-            } else {
-                actor = game.user.character;
-            }
-            let creatureType = e.currentTarget.parentElement.parentElement.parentElement.parentElement.attributes["data-creaturetype"].value;
-            let ability = e.currentTarget.attributes["data-ability"].value;
-
-            let skill = getSkill(creatureType).substring(0, 3).toLowerCase();
-
-            let prof = actor.data.data.skills[skill].prof.flat;
-            let abil = actor.data.data.abilities[ability].mod;
-            let roll = new Roll("1d20 + @prof + @mod", {
-                "prof": prof,
-                "mod": abil
-            });
-
-            await roll.roll();
-
-            let theResult = roll.total;
-
-            let currentTotal = Number(e.currentTarget.parentElement.parentElement.parentElement.parentElement.parentElement.children[3].children[0].children[0].children[1].children[0].innerText);
-            currentTotal += theResult;
-            let chatMessage = game.messages.get(e.currentTarget.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.getAttribute("data-message-id"));
-            let content = chatMessage.data.content;
-            var parser = new DOMParser();
-            var htmlDoc = parser.parseFromString(content, 'text/html');
-            let theElement = htmlDoc.evaluate("//a[@class='hgtmh-total']", htmlDoc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-            //Set Total
-            theElement.singleNodeValue.innerText = currentTotal;
-            theElement = htmlDoc.getElementById(e.currentTarget.id);
-            theElement.innerText = theResult;
-            theElement.setAttribute("data-rolled", "true");
-            theElement.setAttribute("class", "hgtmh-rolled-button");
-			
-			let rollBreakdown = `1d20(${roll.dice[0].total}) + prof(${prof}) + ${ability}(${abil})`;
-            theElement.setAttribute("title", rollBreakdown);
-			
-			// theElement = htmlDoc.evaluate(`(//div[@id='${e.currentTarget.attributes["id"].value}']/../../th)[2]`, htmlDoc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-			// console.log(theElement.singleNodeValue);
-			// console.log(actor.name);
-			// theElement.singleNodeValue.innerText = actor.name.split(" ")[0];
-			
-			theElement = htmlDoc.evaluate(`(//div[@id='${e.currentTarget.attributes["id"].value}']/../../th)[2]`, htmlDoc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-			theElement.innerText = actor.name.split(" ")[0];
-			theElement = htmlDoc.evaluate(`(//div[@id='${e.currentTarget.attributes["id"].value}']/../../th)[3]`, htmlDoc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-			theElement.innerHTML = `<img title="${actor.name}" class="hgtmh-harvest-player-img" src="${actor.img}" />`;
-			
-			
-            //console.log("htmlDoc.singleNodeValue.innerHTML", htmlDoc.children[0].children[1].innerHTML);
-            if (game.user.isGM) {
-                chatMessage.update({
-                    content: htmlDoc.children[0].children[1].innerHTML
-                });
-            } else {
-                game.socket.emit(`module.hgtmh`, {
-                    "action": "updateChatMessage",
-                    "chatMessage": chatMessage.id,
-                    "content": htmlDoc.children[0].children[1].innerHTML
-                });
-            }
-        }
-
-    });
-
-});
 
 function getSkill(type) {
     type = type.split(" ", 1)[0]
         switch (type) {
-        case "Aberration":
+        case "aberration":
             return "Arcana"
-        case "Beast":
+        case "beast":
             return "Survival"
-        case "Celestial":
+        case "celestial":
             return "Religion"
-        case "Construction":
+        case "construction":
             return "Investigation"
-        case "Dragon":
+        case "dragon":
             return "Survival"
-        case "Elemental":
+        case "elemental":
             return "Arcana"
-        case "Fey":
+        case "gey":
             return "Arcana"
-        case "Fiend":
+        case "giend":
             return "Religion"
-        case "Giant":
+        case "giant":
             return "Medicine"
-        case "Humanoid":
+        case "humanoid":
             return "Medicine"
-        case "Monstrosity":
+        case "monstrosity":
             return "Survival"
-        case "Ooze":
+        case "ooze":
             return "Nature"
-        case "Plant":
+        case "plant":
             return "Nature"
-        case "Undead":
+        case "undead":
             return "Medicine"
         default:
             console.log("[ERROR] " + type + " Does not exist")
@@ -191,24 +88,102 @@ function getSkill(type) {
 }
 
 function CreateHarvestMessage(token) {
-	
-	if(token == null){
-		ui.notifications.warn("You must have a token selected to harvest!");
-		return;
-	}
-    let creatureType = token.actor.labels.creatureType;
+    if(token == null){
+        ui.notifications.warn("You must have a token selected to harvest!");
+        return;
+    }
+    let creatureType = token.actor.system.details.type.value;
+    console.log(creatureType)
     if (creatureType == undefined) {
         return;
     }
+    
+    let id1 = Math.floor(Math.random() * 100000000);
+    let id2 = Math.floor(Math.random() * 100000000);
+    let id3 = Math.floor(Math.random() * 100000000);
+    let skillReq = getSkill(creatureType);
+
     let chatData = {
         speaker: ChatMessage.getSpeaker({
             alias: 'Harvest Check'
         }),
-        content: `
-			<div data-hgtmh="true" data-creatureType=${creatureType} data-creatureName=${token.name}>
-			</div>
-		`,
+        content: HARVEST_MESSAGE.replaceAll("{id1}", id1).replaceAll("{id2}", id2).replaceAll("{id3}", id3).replaceAll("{d20}", d20).replaceAll("{creatureType}", creatureType).replaceAll("{creatureName}", token.name).replaceAll("{skillReq}",skillReq),
         "hgtmh": true
     };
+    
+    Hooks.once('renderChatMessage', (chatItem, html) => {    
+        html.find(".hgtmh-roll-button").click(async e => {
+            //console.log(e);
+            if (e.currentTarget.attributes["data-rolled"].value == 'false') {
+                
+                let actor;
+                if (game.user.role == 4) {
+                    actor = canvas.tokens.controlled[0].actor;
+                } else {
+                    actor = game.user.character;
+                }
+                let creatureType = e.currentTarget.parentElement.parentElement.parentElement.parentElement.attributes["data-creaturetype"].value;
+                let ability = e.currentTarget.attributes["data-ability"].value;
+    
+                let skill = getSkill(creatureType).substring(0, 3).toLowerCase();
+    
+                let total = actor.system.skills[skill].total;
+                let mod = actor.system.skills[skill].mod
+                let abil = actor.system.abilities[ability].mod;
+                let roll = new Roll("1d20 + @skill + @ability", {
+                    "skill": total-mod,
+                    "ability": abil
+                });
+    
+                await roll.roll();
+    
+                let theResult = roll.total;
+    
+                let currentTotal = Number(e.currentTarget.parentElement.parentElement.parentElement.parentElement.parentElement.children[3].children[0].children[0].children[1].children[0].innerText);
+                currentTotal += theResult;
+                let chatMessage = game.messages.get(e.currentTarget.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.getAttribute("data-message-id"));
+                let content = chatMessage.content;
+                var parser = new DOMParser();
+                var htmlDoc = parser.parseFromString(content, 'text/html');
+                let theElement = htmlDoc.evaluate("//a[@class='hgtmh-total']", htmlDoc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+                //Set Total
+                theElement.singleNodeValue.innerText = currentTotal;
+                theElement = htmlDoc.getElementById(e.currentTarget.id);
+                theElement.innerText = theResult;
+                theElement.setAttribute("data-rolled", "true");
+                theElement.setAttribute("class", "hgtmh-rolled-button");
+                
+                let rollBreakdown = `1d20(${roll.dice[0].total}) + skill(${total-mod}) + ${ability}(${abil})`;
+                theElement.setAttribute("title", rollBreakdown);
+                
+                // theElement = htmlDoc.evaluate(`(//div[@id='${e.currentTarget.attributes["id"].value}']/../../th)[2]`, htmlDoc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+                // console.log(theElement.singleNodeValue);
+                // console.log(actor.name);
+                // theElement.singleNodeValue.innerText = actor.name.split(" ")[0];
+                
+                theElement = htmlDoc.evaluate(`(//div[@id='${e.currentTarget.attributes["id"].value}']/../../th)[2]`, htmlDoc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+                theElement.innerText = actor.name.split(" ")[0];
+                theElement = htmlDoc.evaluate(`(//div[@id='${e.currentTarget.attributes["id"].value}']/../../th)[3]`, htmlDoc, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+                theElement.innerHTML = `<img title="${actor.name}" class="hgtmh-harvest-player-img" src="${actor.img}" />`;
+                
+                
+                //console.log("htmlDoc.singleNodeValue.innerHTML", htmlDoc.children[0].children[1].innerHTML);
+                if (game.user.isGM) {
+                    chatMessage.update({
+                        content: htmlDoc.children[0].children[1].innerHTML
+                    });
+                } else {
+                    game.socket.emit(`module.hgtmh`, {
+                        "action": "updateChatMessage",
+                        "chatMessage": chatMessage.id,
+                        "content": htmlDoc.children[0].children[1].innerHTML
+                    });
+                }
+            }
+    
+        });
+    })
+    
+
     ChatMessage.create(chatData);
 }
